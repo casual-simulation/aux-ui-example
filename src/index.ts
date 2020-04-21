a06f5a4e7612a471883ea8f0f45a07c37a75ef91
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-04-21 14:34:04 -0400

    feat: Get basic integration with AUX working

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,101 @@
-function component() {
-    const element = document.createElement('div');
+import { BotManager } from '@casual-simulation/aux-vm-browser';
+import { Simulation, AuxConfigParameters } from '@casual-simulation/aux-vm';
+import {
+    PrecalculatedBot,
+    getBotSpace,
+    toast,
+    botAdded,
+    createBot,
+    isBotInDimension,
+} from '@casual-simulation/aux-common';
+import { Subscription } from 'rxjs';
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = ['Test', 'Hello', 'webpack'].join(' ');
+async function start() {
+    const user = {
+        id: 'myUserId',
+        username: 'username',
+        name: 'name',
+        token: 'token',
+        isGuest: false,
+    };
 
-    return element;
+    const universeID = 'http://localhost:3000/dummy/unviverseId';
+
+    const config = {
+        version: 'v1.0.0',
+        versionHash: 'commitHash',
+        device: {
+            supportsAR: false,
+            supportsVR: true,
+        },
+        isBuilder: false,
+        isPlayer: true,
+        builder: null,
+    } as AuxConfigParameters;
+
+    const sim = new BotManager(user, universeID, config);
+
+    await sim.init();
+
+    const app = document.getElementById('app');
+    const div = document.createElement('div');
+    const pre = document.createElement('pre');
+
+    app.appendChild(div);
+    app.appendChild(pre);
+
+    let subs = [] as Subscription[];
+
+    // sim.helper.transaction(
+    //     botAdded(
+    //         createBot(undefined, {
+    //             myCustomFunction: `@player.toast("hi")`,
+    //         }, 'tempLocal')
+    //     )
+    // );
+
+    subs.push(
+        sim.watcher.botsDiscovered.subscribe((bots) => {
+            updateDivText();
+        }),
+        sim.watcher.botsUpdated.subscribe((bots) => {
+            updateDivText();
+        }),
+        sim.watcher.botsRemoved.subscribe((botIds) => {
+            updateDivText();
+        }),
+        sim.localEvents.subscribe((event) => {
+            if (event.type === 'show_toast') {
+                pre.innerText = event.message;
+            }
+        })
+    );
+
+    function updateDivText() {
+        while (div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+
+        const calc = sim.helper.createContext();
+        for (let bot of sim.helper.objects) {
+            if (!isBotInDimension(calc, bot, 'home')) {
+                continue;
+            }
+            const btn = document.createElement('button');
+            btn.addEventListener('click', () => {
+                sim.helper.action('onClick', null);
+            });
+            btn.innerText = `${bot.values.auxLabel}: ${getBotSpace(bot)}`;
+
+            div.appendChild(btn);
+        }
+    }
+
+    function cleanup() {
+        for (let sub of subs) {
+            sub.unsubscribe();
+        }
+    }
 }
 
-document.body.appendChild(component());
+start();
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,21 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: {
+        app: './src/index.ts',
+        vm: path.resolve(
+            __dirname,
+            'node_modules',
+            '@casual-simulation',
+            'aux-vm-browser',
+            'html',
+            'IframeEntry.js'
+        ),
+    },
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -28,7 +38,22 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
+            chunks: ['app'],
             template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new HtmlWebpackPlugin({
+            chunks: ['vm'],
+            // inject: false,
+            // template: path.resolve(
+            //     __dirname,
+            //     'node_modules',
+            //     '@casual-simulation',
+            //     'aux-vm-browser',
+            //     'html',
+            //     'iframe_host.html'
+            // ),
+            title: 'AUX VM',
+            filename: 'aux-vm-iframe.html',
         }),
     ],
 };

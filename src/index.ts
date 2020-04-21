f5a0f4eae277b3f9a83e2960ca0af3bcc6ae65c4
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-04-21 18:05:31 -0400

    feat: Update the example and add some tutorial comments

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import { BotManager } from '@casual-simulation/aux-vm-browser';
-import { Simulation, AuxConfigParameters } from '@casual-simulation/aux-vm';
-import {
-    PrecalculatedBot,
-    getBotSpace,
-    toast,
-    botAdded,
-    createBot,
-    isBotInDimension,
-} from '@casual-simulation/aux-common';
-import { Subscription } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { AuxConfigParameters } from '@casual-simulation/aux-vm';
+import { SubscriptionLike } from 'rxjs';
 import uuid from 'uuid/v4';
 
 async function start() {
@@ -22,44 +13,60 @@ async function start() {
     // Create the user for this session.
     // This tells AUX what ID to use for the player bot
     // and how this session can be reached via remote() actions.
+    //
+    // See https://docs.casualsimulation.com/docs/actions#remoteaction-target
+    // for some more info.
     const user = {
         id: uuid(),
         username: 'username',
         name: 'name',
         token: 'token',
-        isGuest: false,
     };
 
     // The ID of the universe that we are going to load.
-    //
-    const universeID = 'http://localhost:3000/dummy/unviverseId';
-    link.href = 'https://auxplayer.com?auxUniverse=universeId';
+    // Because we're loading from a separate auxPlayer, we
+    // have to include the full URL.
+    const universeID = 'https://auxplayer.com?auxUniverse=universeId';
+    link.href = universeID;
 
+    // The configuration that tells the universe some information
+    // about the environment it is running in.
     const config = {
+        // Information about the application.
         version: 'v1.0.0',
         versionHash: 'commitHash',
+
+        // Information about the device.
         device: {
             supportsAR: false,
             supportsVR: true,
         },
-        isBuilder: false,
-        isPlayer: true,
-        builder: null,
     } as AuxConfigParameters;
 
+    // Create a new simulation for the given user, universe ID, and configuration.
     const sim = new BotManager(user, universeID, config);
 
+    // This triggers the initialization process for the simulation.
+    // The promise resolves once the worker has been setup, but
+    // the simulation might still have setup work to do afterwards.
+    // Use the syncStateChanged observable to be notified when the simulation is completely setup.
     await sim.init();
-
-    let subs = [] as Subscription[];
 
     let initialized = false;
     let wasSynced = false;
 
-    subs.push(
+    let subs = [
+        // syncStateChanged is an event that is triggered
+        // when the sync status changes.
+        // A universe is considered "synced" when it has been initalized and connected to the server.
+        // This is useful for telling whether the universe is likely to have recent data from the server.
+        // Upon subscription, the first callback contains the current sync state.
+        // Note that if the universe hits an error while initializing, the sync event will never fire.
         sim.connection.syncStateChanged.subscribe((synced) => {
             if (synced) {
                 if (!initialized) {
+                    // We're synced, but we haven't run the initialize code yet.
+                    // At this point, it's safe to use every feature in the simulation.
                     initialize();
                     initialized = true;
                 }
@@ -67,27 +74,64 @@ async function start() {
             status.innerText = synced ? 'Connected!' : 'Disconnected.';
             wasSynced = synced;
         }),
+
+        // connectionStateChanged is an event that is triggered
+        // when the connection status to the server changes.
+        // This is reflective of the actual network connection state between the server and the local client.
+        // Useful for monitoring network status.
+        // Upon subscription, the first callback contains the current connection state.
+        sim.connection.connectionStateChanged.subscribe((connected) => {
+            console.warn(connected ? 'Connected' : 'Disconnected');
+        }),
+
+        // botsDiscovered is an event that is triggered
+        // whenever a bot is added to the universe.
+        // Upon subscription, the first callback contains all
+        // the existing bots.
         sim.watcher.botsDiscovered.subscribe((bots) => {
             updateDivText();
         }),
+
+        // botsUpdated is an event that is triggered
+        // whenever a bot is changed in the universe.
         sim.watcher.botsUpdated.subscribe((bots) => {
             updateDivText();
         }),
+
+        // botsRemoved is an event that is triggered
+        // whenever a bot is removed from the universe.
         sim.watcher.botsRemoved.subscribe((botIds) => {
             updateDivText();
         }),
+
+        // Local events is triggered whenever a player action has been has been sent from a script.
+        // Also includes other actions that don't modify the AUX data model.
+        //
+        // See https://docs.casualsimulation.com/docs/actions#player-actions
+        // for a full list of player actions.
         sim.localEvents.subscribe((event) => {
             const li = document.createElement('li');
             li.innerText = `${event.type}`;
             actionList.appendChild(li);
-        })
-    );
+        }),
 
+        // Add the simulation itself to our subscriptions.
+        sim,
+    ] as SubscriptionLike[];
+
+    // Helper function to update the UI with data from the simulation.
+    // In a more complicated application, you would want to respond to
+    // the discovered/removed/update events individually instead of
+    // rebuilding the entire UI on each update.
     function updateDivText() {
         while (botList.firstChild) {
             botList.removeChild(botList.firstChild);
         }
 
+        // Most of the data-related capabilities live in the "helper"
+        // object in the simulation.
+        // In this case, we're grabbing a list of all the bots in the simulation
+        // and iterating over them.
         for (let bot of sim.helper.objects) {
             const li = document.createElement('li');
             li.innerText = bot.id;
@@ -102,6 +146,9 @@ async function start() {
         // 2. Update the player bot with some login information
     }
 
+    // Helper function to cleanup all the subscriptions.
+    // This is useful for shutting down the simulation and all
+    // the resources it is using.
     function cleanup() {
         for (let sub of subs) {
             sub.unsubscribe();

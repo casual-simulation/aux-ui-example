a27152cb107f9c00077c818789cdd8b2cc23c15c
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-04-21 12:00:12 -0400

    chore: Format all files

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,32 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
     ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  devServer: {
-      contentBase: './dist',
-  },
-  plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'src', 'index.html')
-      })
-  ]
 };

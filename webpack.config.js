1bd0502573140ef0dff6329a2f04bc8e45c3e8d1
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-04-21 11:44:10 -0400

    feat: Add Webpack with TypeScript support

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  devServer: {
+      contentBase: './dist',
+  },
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src', 'index.html')
+      })
+  ]
+};
